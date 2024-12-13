e3d536cf93ec55941812838259a5b8f7e0cfc68e
Pierre-Louis Létoquart <randlgint@proton.me>
2024-12-13 16:05:16 -0500

    chore: cleanup and proper formatting

--- core/coreset/model_embed.py
+++ core/coreset/model_embed.py
@@ -1,34 +1,35 @@
-import fiftyone as fo
-import fiftyone.zoo as foz
-import numpy as np
 import os
 import pickle
 
+import fiftyone as fo
+import fiftyone.zoo as foz
+import numpy as np
+
+
 def load_fo_dataset(args):
-    
     if "cifar" in args.dataset:
         dataset = foz.load_zoo_dataset(args.dataset, split="train")
-    else: 
-    
+    else:
         if args.dataset == "imagenet":
-            path = os.path.join(args.data_dir, "imagenet", "ILSVRC", "Data", 
-                                "CLS-LOC", "train")
+            path = os.path.join(
+                args.data_dir, "imagenet", "ILSVRC", "Data", "CLS-LOC", "train"
+            )
         elif "eurosat" in args.dataset:
             path = os.path.join(args.data_dir, args.dataset, "train")
         else:
             raise ValueError(f"{args.dataset} not recognized.")
 
         dataset = fo.Dataset.from_dir(
-            path, 
-            dataset_type=fo.types.ImageClassificationDirectoryTree
+            path, dataset_type=fo.types.ImageClassificationDirectoryTree
         )
 
     return dataset
 
-def load_fo_model(args, model_name):
 
+def load_fo_model(args, model_name):
     if model_name == "clip":
-        model = foz.load_zoo_model("open-clip-torch",
+        model = foz.load_zoo_model(
+            "open-clip-torch",
             clip_model="ViT-L-14",
             pretrained="openai",
         )
@@ -36,12 +37,13 @@ def load_fo_model(args, model_name):
         model = foz.load_zoo_model("resnet18-imagenet-torch")
     elif model_name == "dinov2":
         model = foz.load_zoo_model("dinov2-vitb14-torch")
-    else: model = foz.load_zoo_model(model_name)
-    
+    else:
+        model = foz.load_zoo_model(model_name)
+
     return model
 
-def generate_embedding(args, model_name, embed_file):
 
+def generate_embedding(args, model_name, embed_file):
     dataset = load_fo_dataset(args)
     model = load_fo_model(args, model_name)
 
@@ -50,15 +52,14 @@ def generate_embedding(args, model_name, embed_file):
     os.makedirs(os.path.dirname(embed_file), exist_ok=True)
     pickle.dump(model_embed, open(embed_file, "wb"))
     print(f"Model embeddings saved at {embed_file}.")
-   
+
     return model_embed
 
-def get_model_embedding(args):
 
+def get_model_embedding(args):
     embed_dir = os.path.join(args.data_dir, "preprocess", args.dataset)
-    
+
     for model_name in args.embedding:
-        
         embed_file = os.path.join(embed_dir, f"{model_name}_embedding.pk")
         if os.path.exists(embed_file):
             model_embed = pickle.load(open(embed_file, "rb"))
@@ -66,7 +67,8 @@ def get_model_embedding(args):
             model_embed = generate_embedding(args, model_name, embed_file)
 
         if "total_embed" in locals():
-            total_embed = np.concatenate((total_embed, model_embed), axis=1)
-        else: total_embed = model_embed
+            total_embed = np.concatenate((total_embed, model_embed), axis=1)  # noqa: F821
+        else:
+            total_embed = model_embed
 
     return total_embed
--- core/coreset/utils.py
+++ core/coreset/utils.py
@@ -1,43 +1,42 @@
 import glob
-import numpy as np
 import os
 
-def experiment_name(args):
+import numpy as np
 
+
+def experiment_name(args):
     exp_name = "zcore"
     exp_name += f"-{args.dataset}"
-    for m in args.embedding: exp_name += f"-{m}"
+    for m in args.embedding:
+        exp_name += f"-{m}"
     exp_name += f"-{int(args.n_sample/1000)}Ks"
     exp_name += f"-{args.sample_dim}sd"
-    if args.rand_init: exp_name += "-ri"
+    if args.rand_init:
+        exp_name += "-ri"
     exp_name += f"-{args.redund_nn}nn"
     exp_name += f"-{args.redund_exp}ex"
     exp_name += f"-{args.trial}"
 
-    exp_file = os.path.join(args.results_dir, 
-        args.dataset, 
-        exp_name, 
-        "score.npy"
-    )
+    exp_file = os.path.join(args.results_dir, args.dataset, exp_name, "score.npy")
     os.makedirs(os.path.dirname(exp_file), exist_ok=True)
 
     return exp_name, exp_file
+
 
 def get_trial_list(base_score_dir):
     trial_folders = sorted(glob.glob(base_score_dir + "-*"))
     trials = [f.split(base_score_dir + "-")[-1] for f in trial_folders]
     return trial_folders, trials
 
-def collect_log_data(trial_folders, trials):
 
+def collect_log_data(trial_folders, trials):
     exp_data = {}
     set_types = []
 
     for i, t in enumerate(trials):
-
-        log_folders = sorted(glob.glob(os.path.join(trial_folders[i],"p*")))
-        logs = [os.path.join(f,"train_log.txt") for f in log_folders]
-        acc = [log_validation_accuracy(l) for l in logs]
+        log_folders = sorted(glob.glob(os.path.join(trial_folders[i], "p*")))
+        logs = [os.path.join(f, "train_log.txt") for f in log_folders]
+        acc = [log_validation_accuracy(log) for log in logs]
 
         set_names = [os.path.basename(f) for f in log_folders]
         exp_data[t] = {}
@@ -48,6 +47,7 @@ def collect_log_data(trial_folders, trials):
 
     return exp_data, set_types
 
+
 def log_validation_accuracy(file_name):
     try:
         lines = read_text_file(file_name)
@@ -57,11 +57,11 @@ def log_validation_accuracy(file_name):
         acc = "NA"
     return acc
 
-def make_experiment_log_table(args, trials, exp_data, set_types):
 
+def make_experiment_log_table(args, trials, exp_data, set_types):
     heading = f"Score: {args.base_score_dir}\n{'Setting' : <8}"
     all_results = {}
-    for s in set_types: 
+    for s in set_types:
         heading += f"{s : <8}"
         all_results[s] = []
     print_statements(args.save_file, heading, make_dir=True)
@@ -74,11 +74,11 @@ def make_experiment_log_table(args, trials, exp_data, set_types):
                 r = exp_data[t][s]
                 result += f"{r : <8.2f}"
                 all_results[s].append(r)
-            except: 
+            except:
                 result += f"{'NA' : <8}"
 
         print_statements(args.save_file, result)
- 
+
     print_statements(args.save_file, "\nAggregate Results")
     mean_result = f"{'Mean' : <8}"
     stddev_result = f"{'StdDev' : <8}"
@@ -89,20 +89,20 @@ def make_experiment_log_table(args, trials, exp_data, set_types):
         stddev_result += f"{np.std(all_results[s]) : <8.3f}"
     print_statements(args.save_file, mean_result)
     print_statements(args.save_file, stddev_result)
-    print_statements(args.save_file, 
-                     f"Overall Mean: {np.mean(overall_mean) : <8.2f}\n")
+    print_statements(args.save_file, f"Overall Mean: {np.mean(overall_mean) : <8.2f}\n")
 
     print(f"\nProcessed log table saved at {args.save_file}")
+
 
 def read_text_file(file_name, remove_return=True):
     with open(file_name, "r") as f:
         lines = f.readlines()
     if remove_return:
-        lines = [l.split("\n")[0] for l in lines]
+        lines = [line.split("\n")[0] for line in lines]
     return lines
 
-def print_statements(file_name, statements, add_return=True, make_dir=False):
 
+def print_statements(file_name, statements, add_return=True, make_dir=False):
     if isinstance(statements, str):
         statements = [statements]
     if make_dir:
